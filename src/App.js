5432d3bd9916ae9523fd8be1af3c45ed96aa1892
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-08-17 10:47:14 +0800

    Configured Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <div>
+      <nav className = "navbar bg-light navbar-expand-lg navbar-light">
+        <ul className = "navbaar-nav mr-auto">
+          <li className = "navbar-item">
+            <Link to = "/" className="nav-link">Todos</Link>
+          </li>
+          <li className = "navbar-item">
+            <Link to = "/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
     <Switch>
       <Route exact path="/" component={TodoList}/>
       <Route exact path="/edit/:id" component={EditTodo}/>
       <Route exact path="/create" component={CreateTodo}/>
     </Switch>
+    </div>
   );
 }
 
