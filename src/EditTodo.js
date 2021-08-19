53b68698ae4cd4af8e64fedacfd95efddb425837
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-08-19 21:24:38 +0800

    rearranged the containers and format

--- src/EditTodo.js
+++ src/EditTodo.js
@@ -10,5 +10,17 @@ export const EditTodo = () => {
         })
     }, [])
 
-    return todo ? <TodoForm todo={todo}/> : <div>Loading</div>
-}
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data))
+    }
+
+    return todo ? (
+        <div className="container">
+            <div className = "mt-3">
+                <h3>Edit Todo Item</h3>
+                <TodoForm todo={todo} onSubmit={onSubmit}/>
+            </div>
+        </div> 
+    ):( <div>Loading...</div>
+    );
+};
