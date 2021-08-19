11bc3799448ea82043c6918244340081ec5ac646
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-08-19 21:24:01 +0800

    Updated Todo Form

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,23 +2,21 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 
-export const TodoForm = ({ todo }) => {
+export const TodoForm = ({ todo, onSubmit }) => {
 
     const { register, handleSubmit } = useForm({
         defaultValues: { text: todo ? todo.text : "" },
     });
     const history = useHistory();
     
-    const onSubmit = handleSubmit((data) => {
-        alert(JSON.stringify(data));
-        history.push("/");
+    const submitHandler = handleSubmit((data) => {
+        onSubmit(data)
+        history.push("/");   
     });
     
     return(
-        <div className="container">
-            <div className = "mt-3">
-                <h3>Edit Todo Item</h3>
-                <form onSubmit = {onSubmit}>
+        
+                <form onSubmit = {submitHandler}>
                     <div className="form-group">
                         <label htmlFor = "text">Text:</label>
                         <input 
@@ -31,11 +29,9 @@ export const TodoForm = ({ todo }) => {
                     </div>
                     <div className="form-group">
                         <button type ="submit" className="btn btn-primary">
-                              Create Todo
+                              Save Todo
                         </button>
                     </div>
                 </form>
-            </div>
-        </div>
         );
     };
